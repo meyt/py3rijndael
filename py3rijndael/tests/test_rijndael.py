c8ba75eaa6ac9bc1d7fea465a355f5bf92af95fa
meyt <pasd3000@gmail.com>
2017-11-20 04:06:09 +0330

    Update tests

--- py3rijndael/tests/test_rijndael.py
+++ py3rijndael/tests/test_rijndael.py
@@ -16,10 +16,31 @@ class RijndaelTestCase(unittest.TestCase):
         self.assertEqual(cipher_text, b'Kc8C3vjf+EpLRmgTZ5ckWTzJ/6n7WBHW8pkByDscI/E=')
         self.assertEqual(rijndael.decrypt(cipher), padded_text)
 
-        plain_text = 'lorem lorem lorem la la la..'
-        padded_text = plain_text.ljust(32, b'\x1b'.decode())
-        cipher = rijndael.encrypt(padded_text)
-        self.assertEqual(rijndael.decrypt(cipher), padded_text)
+        # Block size
+        for block_size in (16, 24, 32):
+            rijndael2 = Rijndael(base64.b64decode(key), block_size=block_size)
+            plain_text = 'lorem'
+            padded_text = plain_text.ljust(block_size, b'\x1b'.decode())
+            cipher = rijndael2.encrypt(padded_text)
+            self.assertEqual(rijndael2.decrypt(cipher), padded_text)
+
+        # Exceptions
+        with self.assertRaises(ValueError):
+            plain_text = 'Hey' * 20
+            padded_text = plain_text.ljust(32, b'\x1b'.decode())
+            rijndael.encrypt(padded_text)
+
+        with self.assertRaises(ValueError):
+            Rijndael(base64.b64decode(key), block_size=62)
+
+        with self.assertRaises(ValueError):
+            Rijndael(base64.b64decode(key) * 20, block_size=32)
+
+        with self.assertRaises(ValueError):
+            plain_text = 'Hey'
+            padded_text = plain_text.ljust(32, b'\x1b'.decode())
+            cipher = rijndael.encrypt(padded_text)
+            rijndael.decrypt(cipher * 12)
 
 if __name__ == '__main__':  # pragma: nocover
     unittest.main()
