020a9e3cb28afea2843ecfccaf5a36d293120777
meyt <pasd3000@gmail.com>
2017-11-21 04:38:48 +0330

    Update pkcs7 padding tests
    Using:
    http://programmerin.blogspot.com/2011/08/python-padding-with-pkcs7.html

--- py3rijndael/tests/test_padding.py
+++ py3rijndael/tests/test_padding.py
@@ -12,8 +12,10 @@ class PaddingTestCase(unittest.TestCase):
         self.assertEqual(len(encoded_source), 16)
         self.assertEqual(padding.decode(encoded_source), source)
 
-        # unpad test
+        # Zero length
         padding.decode(b'')
+
+        # Wrong value to decode
         with self.assertRaises(AssertionError):
             padding.decode(b'no-padding')
 
@@ -25,6 +27,25 @@ class PaddingTestCase(unittest.TestCase):
         self.assertEqual(len(encoded_source), 16)
         self.assertEqual(padding.decode(encoded_source), source)
 
+        # Empty string
+        source = b''
+        encoded_source = padding.encode(source)
+        self.assertEqual(encoded_source, b'\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10')
+        self.assertEqual(padding.decode(encoded_source), source)
+
+        # String that is longer than a single block
+        source = b'this string is long enough to span blocks'
+        encoded_source = padding.encode(source)
+        self.assertEqual(encoded_source, b'this string is long enough to span blocks\x07\x07\x07\x07\x07\x07\x07')
+        self.assertEqual(len(encoded_source), 48)
+        self.assertEqual(len(encoded_source) % 16, 0)
+        self.assertEqual(padding.decode(encoded_source), source)
+
+        # Using the max block size
+        padding = Pkcs7Padding(block_size=255)
+        source = b'hi'
+        encoded_source = padding.encode(source)
+        self.assertEqual(len(encoded_source.decode()), 255)
 
 if __name__ == '__main__':  # pragma: nocover
     unittest.main()
