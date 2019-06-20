648c55a4c5c664a673819f60d35ef7cbeaab0a41
meyt <pasd3000@gmail.com>
2019-06-20 10:29:18 +0430

    Drop python 3.3 support

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
